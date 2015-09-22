8af883d0b74b16467cb726482b9ab79fddef337a
Emmanuel Vadot <elbarto@bocal.org>
2015-09-22 12:15:22 +0200

    Use copen for python2 compatibility  Add python versions in classifiers

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ AutoRedis installation script
 """
 
 import os
-
+from codecs import open as copen
 from setuptools import setup
 
 def get_long_description():
     """ Retrieve the long description from DESCRIPTION.rst """
     here = os.path.abspath(os.path.dirname(__file__))
 
-    with open(os.path.join(here, 'README.rst'), encoding='utf-8') as description:
+    with copen(os.path.join(here, 'README.rst'), encoding='utf-8') as description:
         return description.read()
     
 setup(
@@ -29,6 +29,11 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Topic :: Database :: Front-Ends',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     keywords='redis hiredis autoredis',
 )
