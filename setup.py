0fc3f60050d146de43491073eed80eeec71fc240
Emmanuel Vadot <elbarto@bocal.org>
2015-09-22 12:16:13 +0200

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_long_description():
     
 setup(
     name='AutoRedis',
-    version='0.1.0',
+    version='0.2.0',
     description='AutoRedis - Balance your Redis commands accross your master/slaves',
     long_description=get_long_description(),
     author='Emmanuel Vadot',
